d39046ad472766a43393ece40a93094ec2e2dc1f
Crystal Norman <cnorm88@gmail.com>
2020-09-23 15:32:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,17 @@
 class Dog
- 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+  
+ def initialize(name) 
+    @name = name
   end
- 
-  def name
-    @this_dogs_name
+  
+  # Classical get method 
+  def name 
+    @name
+  end
+  
+  # Classical set method 
+  def name=(name) 
+    @name = name
   end
 end
  
